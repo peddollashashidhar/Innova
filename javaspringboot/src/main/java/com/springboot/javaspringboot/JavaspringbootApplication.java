2a7c5bb20eafee96ec01054ce8cc284cc480646d
peddollashashidhar <shashidhar.peddolla@outlook.com>
2025-12-07 17:02:11 +0530

    Add Spring Boot project baseline

---
+++ javaspringboot/src/main/java/com/springboot/javaspringboot/JavaspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.javaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringbootApplication.class, args);
+	}
+
+}
